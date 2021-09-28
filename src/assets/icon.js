21c2a1a4175ccc906a92b4aa79c04f70c2d94ab4
Sefa Kose <sfkse5591@gmail.com>
2021-09-28 13:15:11 +0200

    Design trends section

--- src/assets/icon.js
+++ src/assets/icon.js
@@ -265,6 +265,18 @@ export const SettingsIcon = () => {
         </svg>
     )
 }
+
+export const FeedDots = () => {
+    return (
+        <svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi">
+            <g fill="currentColor">
+                <circle cx="5" cy="12" r="2"></circle>
+                <circle cx="12" cy="12" r="2"></circle>
+                <circle cx="19" cy="12" r="2"></circle>
+            </g>
+        </svg>
+    )
+}
 export const menuLinks = [
     {
         name: "Home",
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,5 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-import { SettingsIcon } from '../assets/icon'
+import { SettingsIcon, FeedDots } from '../assets/icon'
 
 const Feed = () => {
     return (
@@ -11,14 +12,33 @@ const Feed = () => {
                             <svg width="6px" height="6px" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                         </button>
                     </span>
-                    <input type="search" name="q" className="py-3 text-sm text-gray-500 bg-gray-100 flex-1  rounded-full pl-12 focus:outline-none focus:bg-white " placeholder="Search Twitter" />
+                    <input type="search" className="py-3 text-sm text-gray-500 bg-gray-100 flex-1  rounded-full pl-12 focus:bg-white focus:border-blue-light" placeholder="Search Twitter" />
                 </div>
             </form>
-            <div className="flex-col bg-gray-100 rounded-xl p-4">
-                <div className="flex justify-between items-center">
+            <div className="flex-col bg-gray-100 rounded-xl">
+                <div className="flex justify-between items-center p-4">
                     <h2 className="text-xl font-bold">Trends for you</h2>
-                    <span className="p-1 hover:bg-gray-hover rounded-full transition-all"> <SettingsIcon /></span>
+                    <span className="p-1 hover:bg-gray-hover rounded-full transition-all">
+                        <SettingsIcon />
+                    </span>
                 </div>
+                <div >
+                    <ul >
+                        <li className="hover:bg-gray-200 py-3 px-4 flex-col transition-all ">
+                            <div className="flex justify-between items-center">
+                                <p className="text-xs text-gray-500">Trending in Sweden</p>
+                                <span className="p-1 hover:bg-blue-iconHover hover:text-blue-500 rounded-full" > <FeedDots /></span>
+                            </div>
+                            <p className="font-bold">Goteborg</p>
+                            <p className="text-xs text-gray-500">25.5K Tweets</p>
+
+                        </li>
+                        <li className="hover:bg-gray-200 py-3 px-4 rounded-br-xl rounded-bl-xl flex-col transition-all ">
+                            <a href="#" className="text-blue-light  text-sm">Show More</a>
+                        </li>
+                    </ul>
+                </div>
+
             </div>
         </div>
     )
